998e199d4c7dcce9d00588a09b0c6192fb39ee2d
Brian Bolnick <brianbolnick@gmail.com>
2024-06-04 14:33:02 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo-full-white.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{ height: "50px" }}
+        />
+        <div style={{ display: "flex", gap: 12, flexDirection: "column" }}>
+          <p style={{ fontSize: 24 }}>
+            The job placement and outcome tracking services of Jobwise are no
+            longer supported. We apologize for any inconvenience.
+            <br />
+            You may request a data export of your outcome information by
+            reaching out to support@jobwise.com.
+          </p>
+        </div>
       </header>
     </div>
   );
